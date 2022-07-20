7de908ec51e3c66490c3f5cdac05c21987419a96
Nicholas Mateus <nicholas.mateus@gmail.com>
2022-07-20 20:39:36 -0300

    implementacao de endpoints customizados

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import com.ead.authuser.enums.UserStatus;
 import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
+import com.fasterxml.jackson.annotation.JsonView;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import org.springframework.beans.BeanUtils;
@@ -29,7 +30,8 @@ public class AuthenticationController implements AuthenticationControllerAPI {
 
     @Override
     @PostMapping("/signup")
-    public ResponseEntity<Object> registerUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<Object> registerUser(@RequestBody
+                                               @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto) {
         if (userService.existsByUsername(userDto.getUsername())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!");
         }
--- authuser/src/main/java/com/ead/authuser/controllers/UserControllerAPI.java
+++ authuser/src/main/java/com/ead/authuser/controllers/UserControllerAPI.java
@@ -1,5 +1,6 @@
 package com.ead.authuser.controllers;
 
+import com.ead.authuser.dtos.UserDto;
 import com.ead.authuser.models.UserModel;
 import java.util.List;
 import java.util.UUID;
@@ -8,8 +9,9 @@ import org.springframework.http.ResponseEntity;
 public interface UserControllerAPI {
 
     ResponseEntity<List<UserModel>> getAllUsers();
-
     ResponseEntity<Object> getOneUser(UUID userId);
-
     ResponseEntity<Object> deleteUser(UUID userId);
+    ResponseEntity<Object> updateUser(UUID userId, UserDto userDto);
+    ResponseEntity<Object> updatePassword(UUID userId, UserDto userDto);
+    ResponseEntity<Object> updateImage(UUID userId, UserDto userDto);
 }
