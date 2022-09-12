f14e818cfc3e5ec5e998f91047949707461f48db
Nicholas Mateus <nicholas.mateus@gmail.com>
2022-09-11 22:45:28 -0300

    Tipo User Instructor para Criar Courses

---
+++ authuser/src/main/java/com/ead/authuser/controllers/InstructorAPI.java
@@ -0,0 +1,11 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.dtos.InstructorDto;
+import javax.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface InstructorAPI {
+
+    ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto);
+}
---
+++ authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -0,0 +1,46 @@
+package com.ead.authuser.controllers;
+
+import com.ead.authuser.dtos.InstructorDto;
+import com.ead.authuser.enums.UserType;
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.UserService;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+import javax.validation.Valid;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/instructors")
+public class InstructorController implements InstructorAPI {
+
+    private final UserService userService;
+
+    public InstructorController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/subscription")
+    public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
+        Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
+        if (!userModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        } else {
+            var userModel = userModelOptional.get();
+            userModel.setUserType(UserType.INSTRUCTOR);
+            userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userService.save(userModel);
+            return ResponseEntity.status(HttpStatus.OK).body(userModel);
+        }
+    }
+
+}
