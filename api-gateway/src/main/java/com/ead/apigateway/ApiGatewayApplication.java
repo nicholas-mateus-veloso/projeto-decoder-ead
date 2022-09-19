42f3f32a40da1a23a7a652f6b237943f15f18d4e
Nicholas Mateus <nicholas.mateus@gmail.com>
2022-09-19 11:19:05 -0300

    Configurar Gateway com Eureka Server e Roteamentos

--- api-gateway/src/main/java/com/ead/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ead/apigateway/ApiGatewayApplication.java
@@ -2,12 +2,14 @@ package com.ead.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
 }
